207309c7a11743b454c61ed741215db281bb1d95
Break Yang <breakds@gmail.com>
2020-04-27 09:59:08 -0700

    Implement the vga buffer utilities, without newline.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 // Quote: By default, Rust uses unwinding to run the destructors of
@@ -11,21 +13,11 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     // The linker looks for a function named "_start" by default
 
-    // Cast to a raw pointer.
-    let vga_buffer = 0xb8000 as *mut u8;
+    vga_buffer::write_something();
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-    
     loop {}
 }
